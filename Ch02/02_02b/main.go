c7d84c33ca0b428122ea1cf2691cbda667c43c17
Rohan Rathi <51408891+RohanIRathi@users.noreply.github.com>
2025-02-10 23:58:25 +0000

    Second task

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,17 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	stringln, err := fmt.Println("The value is", aNumber)
+	if err == nil {
+		fmt.Println("String length:", stringln)
+	}
+
+	fmt.Printf("Value of number: %v\n", aNumber)
+	fmt.Printf("Type of number: %T\n", aNumber)
 }
