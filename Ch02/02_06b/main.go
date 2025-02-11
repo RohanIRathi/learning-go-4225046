4581e62944927b45e8d1365998450b6a3fb992af
Rohan Rathi <51408891+RohanIRathi@users.noreply.github.com>
2025-02-11 00:17:21 +0000

    Sixth task

--- Ch02/02_06b/main.go
+++ Ch02/02_06b/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Printf("Go was announced at %s\n", t)
+
+	n := time.Now()
+	fmt.Printf("The time currently is %s\n", n)
+	fmt.Printf("The object type is %T\n", n)
+	fmt.Print(n.Format(time.ANSIC) + "\n")
+
+	tomorrow := n.AddDate(0, 0, 1)
+	fmt.Print(tomorrow.Format(time.ANSIC) + "\n")
+
+	format := "Mon 2006-02-01"
+	fmt.Print(tomorrow.Format(format) + "\n")
 
 }
