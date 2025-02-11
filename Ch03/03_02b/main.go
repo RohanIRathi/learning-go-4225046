007f49a9ec25a565689aec02fd831135e7c848e8
Rohan Rathi <51408891+RohanIRathi@users.noreply.github.com>
2025-02-11 00:23:03 +0000

    Seventh task

--- Ch03/03_02b/main.go
+++ Ch03/03_02b/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	var p *int = &anInt
+	if p == nil {
+		fmt.Println("p is nil")
+	} else {
+		fmt.Println("Value of p:", *p)
+	}
+
+	value1 := 42.13
+	pointer1 := &value1
+	*pointer1 = *pointer1 / 2
+	fmt.Println("Pointer Value:", *pointer1)
+	fmt.Println("Value 1:", value1)
 }
