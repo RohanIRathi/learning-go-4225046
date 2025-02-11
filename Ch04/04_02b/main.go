62a9ad8d5f103d69878b07231fee6668b31a9ff0
Rohan Rathi <51408891+RohanIRathi@users.noreply.github.com>
2025-02-11 00:51:44 +0000

    Thirteenth task

--- Ch04/04_02b/main.go
+++ Ch04/04_02b/main.go
@@ -10,7 +10,37 @@ func main() {
 	weekday := time.Now().Weekday()
 	fmt.Printf("Today is %v\n", weekday)
 
-	dayNumber := int(weekday)
+	dayNumber := 0
 	fmt.Printf("The day as a number is %v\n", dayNumber)
+
+	var result string
+	switch dayNumber {
+	case 1:
+		result = "It's a Monday!"
+	case 2:
+		result = "It's a Tuesday!"
+	case 3:
+		result = "It's a Wednesday!"
+	case 4:
+		result = "It's a Thursday!"
+	case 5:
+		result = "It's a Friday!"
+	default:
+		result = "It's the weekend!"
+	}
+	fmt.Println(result)
+
+	x := -42
+	switch {
+	case x < 0:
+		result = "Less than zero"
+		fallthrough
+	case x > 0:
+		result = "Greater than zero"
+		fallthrough
+	default:
+		result = "Equal to zero"
+	}
+	fmt.Println(result)
 
 }
